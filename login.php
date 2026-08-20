407311f9504d6328151d4ce7546f587bb84c4730
Daksh110306 <dakshnandanwar2141@gmail.com>
2026-08-21 00:18:50 +0530

    Connect admin login to online database

--- login.php
+++ login.php
@@ -2,16 +2,7 @@
 
 session_start();
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "crime_analysis";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Database connection failed: " . $conn->connect_error);
-}
+require_once "db.php";
 
 $error = "";
 
