2201e6c0fdd262d8410eb3b36d616a735c52d6df
Daksh110306 <dakshnandanwar2141@gmail.com>
2026-08-21 00:20:06 +0530

    Connect admin dashboard to online database

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -10,25 +10,9 @@ if (
     exit;
 }
 
-/* DATABASE CONNECTION */
+/* ONLINE DATABASE CONNECTION */
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "crime_analysis";
-
-$conn = new mysqli(
-    $servername,
-    $username,
-    $password,
-    $dbname
-);
-
-if ($conn->connect_error) {
-    die("Database connection failed: " . $conn->connect_error);
-}
-
-$conn->set_charset("utf8mb4");
+require_once "db.php";
 
 /* GET ALL CRIME RECORDS */
 
