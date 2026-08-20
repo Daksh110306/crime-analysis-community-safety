5a16771f73700903156c0bb1828790df0f4f7af6
Daksh110306 <dakshnandanwar2141@gmail.com>
2026-08-21 00:13:26 +0530

    Connect crime data to online database

--- crime_data.php
+++ crime_data.php
@@ -1,17 +1,6 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "crime_analysis";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Database connection failed: " . $conn->connect_error);
-}
-
-$conn->set_charset("utf8mb4");
+require_once "db.php";
 
 /* Recent crime records first */
 
